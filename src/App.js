77f5476317091201a06a34f212bece3c4f795e6f
Md Yeamin <ahmedyeamin05@gmail.com>
2022-06-29 17:07:13 +0600

    try

--- src/App.js
+++ src/App.js
@@ -20,15 +20,17 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route index element={<Home />} />
-        <Route path='home' element={<Home />}>
-          <Route path='one' element={<One />} />
-          <Route path='two' element={<Two />} />
-        </Route>
-
+        
         <Route path='courses' element={<Courses />} />
         <Route path='resources' element={<Resources />} />
         <Route path='dashboard' element={<Classes />} />
         <Route path='checkout' element={<EnrollNow />} />
+        <Route path='home' element={<Home />}>
+          <Route index element={<One />} />
+          <Route path='one' element={<One />} />
+          <Route path='two' element={<Two />} />
+          <Route path='*' element={<NoMatch />} />
+        </Route>
         <Route path='*' element={<NoMatch />} />
       </Routes>
       <Footer />
